f595124e1f2df3792e929ec5c0e2df55a675598c
ArshavineRoy <arshavinewaema@gmail.com>
2023-07-30 16:00:21 +0300

    add removeFromFavorites to Favorites
     - remove bots from Favorites when the Remove button is clicked
    
     - style favorites cards to flex-start

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -1,39 +1,42 @@
-function Favorites({ favorites }) {
+function Favorites({ favorites, removeFromFavorites }) {
   return (
     <>
-      <h3>Favorites</h3>
-      <div className="favorites display-bots flex-wrap">
-        {favorites.map(({ id, name, bot_class, catchphrase, avatar_url }) => (
-          <div className="card" key={id}>
-            <img
-              src={avatar_url}
-              className="card-img-top bot-specs"
-              alt="bot image"
-            />
-            <div className="card-body bot-specs">
-              <h5 className="card-title">
-                {name}  
-                <span>
-                    <i
-                    className="fa-solid fa-copyright fa-beat-fade"
-                    style={{ color: "#ff006f" }}
-                  ></i>  
-                    {bot_class}
-                </span>
-              </h5>
-              <p className="card-text">{catchphrase}</p>
-            </div>
-            <div className="card-body d-flex justify-content-between">
-              <a href="#" className="btn btn-success">
-                Enlist
-              </a>
-              <a href="#" className="btn btn-info">
-                Remove
-              </a>
-            </div>
-          </div>
-        ))}
-      </div>
+        <h3>Favorites</h3>
+        <div className="favorites flex-wrap">
+            {favorites.map(({ id, name, bot_class, catchphrase, avatar_url }) => (
+                <div className="card" key={id}>
+                    <img
+                    src={avatar_url}
+                    className="card-img-top bot-specs"
+                    alt="bot image"
+                    />
+                    <div className="card-body bot-specs">
+                    <h5 className="card-title">
+                        {name}  
+                        <span>
+                            <i
+                            className="fa-solid fa-copyright fa-beat-fade"
+                            style={{ color: "#ff006f" }}
+                        ></i>  
+                            {bot_class}
+                        </span>
+                    </h5>
+                    <p className="card-text">{catchphrase}</p>
+                    </div>
+                    <div className="card-body d-flex justify-content-between">
+                    <a href="#" className="btn btn-success">
+                        Enlist
+                    </a>
+                    <a href="#" className="btn btn-info" onClick={(e) => {
+                        e.preventDefault();
+                        removeFromFavorites(id)}}
+                        >
+                        Remove
+                    </a>
+                    </div>
+                </div>
+            ))}
+        </div>
     </>
   );
 }
