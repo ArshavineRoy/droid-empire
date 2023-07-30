afd7f9517701873d1e411953e33435ab01d67ee7
ArshavineRoy <arshavinewaema@gmail.com>
2023-07-30 20:52:04 +0300

    write handleFilterChange based on checked boxes
    - useState to manage state of checked/selected selected filters
    
    - useEffect to call the onFilterChange prop whenever the selectedFilters
    state changes. Allows to communicate the selected filters back to Pages.

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,103 +1,133 @@
-import React from 'react';
+import React,  { useState } from 'react';
 
-function Filter () {
-  return (
-    <div className="container ms-0 my-5 filter" id="filterContent">
-      <h4>Filter by class</h4>
-      <div className="d-flex justify-content-between mt-3">
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value="Support"
-            id="chkSupport"
-          />
-          <label className="form-check-label" htmlFor="chkSupport">
-            Support
-            <i
-              className="fa-solid fa-jet-fighter fa-beat-fade"
-              style={{ color: '#d30993' }}
-            ></i>
-          </label>
+function Filter ({ onFilterChange }) {
+    const [selectedFilters, setSelectedFilters] = useState([]);
+
+    const handleFilterChange = (e) => {
+        const { value, checked } = e.target;
+        if (checked) {
+          setSelectedFilters((prevSelectedFilters) => [...prevSelectedFilters, value]);
+        } else {
+          setSelectedFilters((prevSelectedFilters) => prevSelectedFilters.filter((filter) => filter !== value));
+        }
+    };
+
+    useEffect(() => {
+        if (onFilterChange) {
+          onFilterChange(selectedFilters);
+        }
+    }, [selectedFilters, onFilterChange]);
+
+
+    return (
+        <div className="container ms-0 my-5 filter" id="filterContent">
+        <h4>Filter by class</h4>
+        <div className="d-flex justify-content-between mt-3">
+            <div className="form-check">
+            <input
+                className="form-check-input"
+                type="checkbox"
+                value="Support"
+                id="chkSupport"
+                checked={selectedFilters.includes('Support')}
+                onChange={handleFilterChange}
+            />
+            <label className="form-check-label" htmlFor="chkSupport">
+                Support
+                <i
+                className="fa-solid fa-jet-fighter fa-beat-fade"
+                style={{ color: '#d30993' }}
+                ></i>
+            </label>
+            </div>
+            <div className="form-check">
+            <input
+                className="form-check-input"
+                type="checkbox"
+                value="Medic"
+                id="chkMedic"
+                checked={selectedFilters.includes('Medic')}
+                onChange={handleFilterChange}
+            />
+            <label className="form-check-label" htmlFor="chkMedic">
+                Medic
+                <i
+                className="fa-solid fa-briefcase-medical fa-beat-fade"
+                style={{ color: '#d30993' }}
+                ></i>
+            </label>
+            </div>
+            <div className="form-check">
+            <input
+                className="form-check-input"
+                type="checkbox"
+                value="Assault"
+                id="chkAssault"
+                checked={selectedFilters.includes('Assault')}
+                onChange={handleFilterChange}
+            />
+            <label className="form-check-label" htmlFor="chkAssault">
+                Assault
+                <i
+                className="fa-solid fa-gun fa-beat-fade"
+                style={{ color: '#d30993' }}
+                ></i>
+            </label>
+            </div>
+            <div className="form-check">
+            <input
+                className="form-check-input"
+                type="checkbox"
+                value="Defender"
+                id="chkDefender"
+                checked={selectedFilters.includes('Defender')}
+                onChange={handleFilterChange}
+            />
+            <label className="form-check-label" htmlFor="chkDefender">
+                Defender
+                <i
+                className="fa-solid fa-shield-halved fa-beat-fade"
+                style={{ color: '#d30993' }}
+                ></i>
+            </label>
+            </div>
+            <div className="form-check">
+            <input
+                className="form-check-input"
+                type="checkbox"
+                value="Captain"
+                id="chkCaptain"
+                checked={selectedFilters.includes('Captain')}
+                onChange={handleFilterChange}
+            />
+            <label className="form-check-label" htmlFor="chkCaptain">
+                Captain
+                <i
+                className="fa-solid fa-copyright fa-beat-fade"
+                style={{ color: '#d30993' }}
+                ></i>
+            </label>
+            </div>
+            <div className="form-check">
+            <input
+                className="form-check-input"
+                type="checkbox"
+                value="Witch"
+                id="chkWitch"
+                checked={selectedFilters.includes('Witch')}
+                onChange={handleFilterChange}
+            />
+            <label className="form-check-label" htmlFor="chkWitch">
+                Witch
+                <i
+                className="fa-solid fa-hat-wizard fa-beat-fade"
+                style={{ color: '#d30993' }}
+                ></i>
+            </label>
+            </div>
         </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value="Medic"
-            id="chkMedic"
-          />
-          <label className="form-check-label" htmlFor="chkMedic">
-            Medic
-            <i
-              className="fa-solid fa-briefcase-medical fa-beat-fade"
-              style={{ color: '#d30993' }}
-            ></i>
-          </label>
         </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value="Assault"
-            id="chkAssault"
-          />
-          <label className="form-check-label" htmlFor="chkAssault">
-            Assault
-            <i
-              className="fa-solid fa-gun fa-beat-fade"
-              style={{ color: '#d30993' }}
-            ></i>
-          </label>
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value="Defender"
-            id="chkDefender"
-          />
-          <label className="form-check-label" htmlFor="chkDefender">
-            Defender
-            <i
-              className="fa-solid fa-shield-halved fa-beat-fade"
-              style={{ color: '#d30993' }}
-            ></i>
-          </label>
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value="Captain"
-            id="chkCaptain"
-          />
-          <label className="form-check-label" htmlFor="chkCaptain">
-            Captain
-            <i
-              className="fa-solid fa-copyright fa-beat-fade"
-              style={{ color: '#d30993' }}
-            ></i>
-          </label>
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value="Witch"
-            id="chkWitch"
-          />
-          <label className="form-check-label" htmlFor="chkWitch">
-            Witch
-            <i
-              className="fa-solid fa-hat-wizard fa-beat-fade"
-              style={{ color: '#d30993' }}
-            ></i>
-          </label>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Filter;
